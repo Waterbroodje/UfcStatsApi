24a0a4a216085cc9730aff3bdf076d6b2be3f175
timo <wortelschaaf.info@gmail.com>
2025-01-28 11:13:31 +0100

    Pushed

--- src/main/java/me/waterbroodje/ufcstatsapi/service/RefereeService.java
+++ src/main/java/me/waterbroodje/ufcstatsapi/service/RefereeService.java
@@ -1,11 +1,23 @@
 package me.waterbroodje.ufcstatsapi.service;
 
-import me.waterbroodje.ufcstatsapi.repository.FighterRepository;
+import me.waterbroodje.ufcstatsapi.model.Referee;
+import me.waterbroodje.ufcstatsapi.repository.RefereeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RefereeService {
 
-    @Autowired public RefereeService refereeService;
+    @Autowired public RefereeRepository refereeRepository;
+
+    public Optional<Referee> getReferee(Long id) {
+        return refereeRepository.findById(id);
+    }
+
+    public List<Referee> getAll() {
+        return refereeRepository.findAll();
+    }
 }
